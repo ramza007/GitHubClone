6fe84c64678cfecb0411d4767811499814136057
ramza007 <ramzalvo007@gmail.com>
2018-03-24 15:25:26 +0300

    deleted service folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,3 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-// import { HttpClientModule } from '@angular/common/http';
-// import { FormsModule } from '@angular/forms'
-
-
-// import { AppComponent } from './app.component';
-// import { ProfileComponent } from './profile/profile.component';
-// import { ServicesComponent } from './services/services.component';
-
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     ProfileComponent,
-//     ServicesComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     HttpClientModule,
-//     FormsModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -33,13 +5,11 @@ import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { ServicesComponent } from './services/services.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
