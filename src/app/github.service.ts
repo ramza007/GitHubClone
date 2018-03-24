897a79955b0013aa9efbd36f87b26f007202677f
ramza007 <ramzalvo007@gmail.com>
2018-03-24 14:44:08 +0300

    added github services

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,34 +1,29 @@
 import { Injectable } from '@angular/core';
-import {HttpClient}  from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
-import {User} from './user'
-import {Repos} from './repos'
-import {environment} from '../environments/environment'
-
-
-
-
-
+import { User } from './user'
+import { Repos } from './repos'
+import { environment } from '../environments/environment'
 @Injectable()
 export class GithubService {
-//  private apiUrl :string = environment.apiUrl;
-  username:string;
+  //  private apiUrl :string = environment.apiUrl;
+  username: string;
 
-  constructor(private _http: HttpClient){
+  constructor(private _http: HttpClient) {
     console.log('Github Service Ready...');
-    this.username =  'ramza007';
+    this.username = 'ramza007';
 
   }
-  getUser(){
-    return this._http.get("https://api.github.com/users/"+this.username)
-    .map(result=>result)
+  getUser() {
+    return this._http.get("https://api.github.com/users/" + this.username)
+      .map(result => result)
 
   }
-  getRepos(){
-    return this._http.get("https://api.github.com/users/"+this.username+'/repos')
-    .map(result=>result)
-}
-updateUser(username:string){
-  this.username = username;
-}
+  getRepos() {
+    return this._http.get("https://api.github.com/users/" + this.username + '/repos')
+      .map(result => result)
+  }
+  updateUser(username: string) {
+    this.username = username;
+  }
 }
