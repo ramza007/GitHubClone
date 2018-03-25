e8abd5ed4f1759e8f8275e98e8c49d8f5d4108ba
ramza007 <ramzalvo007@gmail.com>
2018-03-25 08:07:57 +0300

    modified and corrected all ts files

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,31 +8,31 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  user:any=[];
-  repos:any=[];
-  username:string;
+  user: any= [];
+  repos: any= [];
+  username: string;
   constructor(private _githubService: GithubService) {
     this._githubService.getUser().subscribe(user => {
       console.log(user);
       this.user = user;
-    })
+    });
     this._githubService.getRepos().subscribe(repos => {
-      //console.log(user);
+      // console.log(user);
       this.repos = repos;
-    })
+    });
   }
   ngOnInit() {
   }
-   searchUser(){
+   searchUser() {
      this._githubService.updateUser(this.username);
      this._githubService.getUser().subscribe(user => {
        this.user = user;
-     })
-     //console.log('It works');
+     });
+     // Console.log('It works');
      this._githubService.getRepos().subscribe(repos => {
-       //console.log(user);
+       // Console.log(user);
        this.repos = repos;
-     })
+     });
 
    }
 }
